7a328844c484f3317bb11184d88f67aaa8be756a
Sallommea <salome.chkhaidze94@gmail.com>
2024-05-29 06:39:41 +0200

    Tasks 1 and 2 completed

---
+++ TodoListApp.Services.Database/TodoListDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoListApp.Services.Database;
+public class TodoListDbContext : DbContext
+{
+    public TodoListDbContext(DbContextOptions<TodoListDbContext> options)
+           : base(options)
+    {
+    }
+}
